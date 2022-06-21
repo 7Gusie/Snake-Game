d58067dad4b2d76c78a546a536e6b72417c332a6
Gustavo dos Santos Lopes <94156483+7Gusie@users.noreply.github.com>
2022-06-21 15:11:51 -0300

    Update snake_main.py
    Adicionado:
        Criação de um arquivo secundário
        Finalização do mecanismo de movimentação da Cobrinha

--- snake_main.py
+++ snake_main.py
@@ -1,89 +1,91 @@
-import sys, random, pygame as pg
-
-pg.init()
-pg.font.init()
-
-window = pg.display.set_mode([1200, 900])
-pg.display.set_caption("Snake Game")
-
-font = pg.font.SysFont("Lucida Console", 30)
+import sys, random
+from snake_assets import pg, window, font, color_indigo, score
 
 clock = pg.time.Clock()
 
-color_indigo = (75, 0, 130)
+black = (0, 0, 0)
 
 snake_position = [600, 450]
 snake_size = [(100, 50), (90, 50), (80, 50)]
 
-apple_position = [random.randrange(1, 120 * 10), random.randrange(1 , 90 * 10)] 
+apple_position = [random.randrange(1, 100) * 10, random.randrange(1 , 70) * 10] 
 apple_spawn = True
 
 direction = 'RIGHT'
 change_to = direction
 
-score = 0
-
-def showScore():
-    showScore_surface = font.render("Pontuação: " + str(score), True, color_indigo)
-    window.blit(showScore_surface, (200, 200))
-
+def showScore(option, color_indigo, font):
+    window.blit(font.render("Pontuação: " + str(score), True, color_indigo), (10, 10))
 
 def gameOver():
-    gameOver_text = font.render("VOCÊ PERDEU!", True, color_indigo)
-    window.blit(gameOver_text, (300, 300))
-    showScore()
+    window.fill(black)
+    window.blit(font.render("VOCÊ PERDEU!", True, color_indigo), (300, 300))
+    showScore(1, color_indigo, font)
 
-def main():
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_w or event.key == pg.K_UP:
+                change_to = "UP"
+            if event.key == pg.K_s or event.key == pg.K_DOWN:
+                change_to = "DOWN"
+            if event.key == pg.K_a or event.key == pg.K_LEFT:
+                change_to = "LEFT"
+            if event.key == pg.K_d or event.key == pg.K_RIGHT:
+                change_to = "RIGHT"
+            if event.key == pg.K_ESCAPE:
                 pg.quit()
                 sys.exit()
-
-            elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_w or event.key == pg.K_UP:
-                    change_to = "UP"
-                if event.key == pg.K_s or event.key == pg.K_DOWN:
-                    change_to = "DOWN"
-                if event.key == pg.K_a or event.key == pg.K_LEFT:
-                    change_to = "UP"
-                if event.key == pg.K_d or event.key == pg.K_RIGHT:
-                    change_to = "DOWN"
-                if event.key == pg.K_ESCAPE:
-                    pg.quit()
-                    sys.exit()
         
-        if change_to == "UP" and direction != "DOWN":
-            direction = "UP"
-        if change_to == 'DOWN' and direction != 'UP':
-            direction = 'DOWN'
-        if change_to == 'LEFT' and direction != 'RIGHT':
-            direction = 'LEFT'
-        if change_to == 'RIGHT' and direction != 'LEFT':
-            direction = 'RIGHT'
+    if change_to == "UP" and direction != "DOWN":
+        direction = "UP"
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
 
-        if direction == "UP":
-            snake_position[1] -= 10
-        if direction == 'DOWN':
-            snake_position[1] += 10
-        if direction == 'LEFT':
-            snake_position[0] -= 10
-        if direction == 'RIGHT':
-            snake_position[0] += 10
+    if direction == "UP":
+        snake_position[1] -= 10
+    if direction == 'DOWN':
+        snake_position[1] += 10
+    if direction == 'LEFT':
+        snake_position[0] -= 10
+    if direction == 'RIGHT':
+        snake_position[0] += 10
 
-        snake_size.insert(0, list(snake_position))
-        if snake_position[0] == apple_position[0] and snake_position[1] == apple_position[1]:
-            score += 1
-            apple_spawn = False
-        else:
-            snake_size.pop()
+    snake_size.insert(0, list(snake_position))
+    if snake_position[0] == apple_position[0] and snake_position[1] == apple_position[1]:
+        score += 1
+        apple_spawn = False
+    else:
+        snake_size.pop()
 
-        if not apple_spawn:
-            apple_position = [random.randrange(1, (1200//10)) * 10, random.randrange(1, (900//10)) * 10]
-            apple_spawn = True
+    if not apple_spawn:
+        apple_position = [random.randrange(1, 100) * 10, random.randrange(1, 70) * 10]
+        apple_spawn = True
 
+    window.fill(black)
+    for pos in snake_size:
+        pg.draw.rect(window, color_indigo, pg.Rect(pos[0], pos[1], 10, 10))
+        
+    pg.draw.rect(window, color_indigo, pg.Rect(apple_position[0], apple_position[1], 10, 10))
+        
+    if snake_position[0] < 0 or snake_position[0] > 990:
+        gameOver()
+    if snake_position[1] < 0 or snake_position[1] > 690:
+        gameOver()
 
-        clock.tick(60)
-        pg.display.update()
+    for block in snake_size[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+            gameOver()
 
-main()
+    showScore(1, color_indigo, font)
+
+    clock.tick(20)
+    pg.display.update()
